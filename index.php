6e823e438a3fa7bee818d9c17aa7498890819348
Gustav Godsk laptop <gustavgodsk@gmail.com>
2021-04-27 18:12:00 +0200

    from laptop

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     include "includes/header.php";
-    echo "hello world lol!"
+    echo "hello world lol!";
+    echo "changes from laptop!!!hahaha";
 ?>
 
 <script src="js/script.js"></script>
